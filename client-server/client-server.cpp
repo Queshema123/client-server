317cdfb8943192b1c1c5470197b4a7113568713a
Queshema123 <vladimir.borisov.1230@gmail.com>
2024-11-19 13:36:48 +0300

    Добавьте файлы проекта.

---
+++ client-server/client-server.cpp
@@ -0,0 +1,12 @@
+﻿// client-server.cpp: определяет точку входа для приложения.
+//
+
+#include "client-server.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
